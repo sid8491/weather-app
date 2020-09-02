e7a8f7ab89e164e603abafb5238b6ee4e8d43118
Sid <sid.8491@gmail.com>
2020-09-03 01:24:44 +0530

    Updates

---
+++ precache-manifest.b3bfe3f6107412c85d54d7be0b80d655.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af55c43e7eb4ad1111be2ef9d593489b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "4053c0c240e8a00aeffa",
+    "url": "/weather-app/static/css/main.9804caec.chunk.css"
+  },
+  {
+    "revision": "d36834ddd497d19b90df",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.2e93365d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4053c0c240e8a00aeffa",
+    "url": "/weather-app/static/js/main.2b1f8621.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "62d20442ec195c5a48b129c86a4a98cd",
+    "url": "/weather-app/static/media/cloudy-weather.62d20442.jpg"
+  },
+  {
+    "revision": "edd368b439f561aefd41734e6c78c012",
+    "url": "/weather-app/static/media/cold-weather.edd368b4.jpg"
+  },
+  {
+    "revision": "6c1cf5a87c29da693db16c60e6a0757b",
+    "url": "/weather-app/static/media/hot-weather.6c1cf5a8.jpg"
+  },
+  {
+    "revision": "79ab941bd52eb0aacdff0ffe2d7757cd",
+    "url": "/weather-app/static/media/lightning-weather.79ab941b.jpg"
+  },
+  {
+    "revision": "b11277a7dd921bb77b78fe0c8f327008",
+    "url": "/weather-app/static/media/spring-weather.b11277a7.jpg"
+  }
+]);
